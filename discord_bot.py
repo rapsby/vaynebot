34c84636547058ff355a13af176257b8efdeabd0
rapsby <38773410+rapsby@users.noreply.github.com>
2020-03-09 23:03:24 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -29,5 +29,82 @@ async def on_message(message):
         embed.set_image(url = urlF)
         await client.send_message(message.channel, embed=embed)
 
+    if message.content.startswith("!재생"):
+
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        msg1 = message.content.split(" ")
+        url = msg1[1]
+        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+        print(player.is_playing())
+        players[server.id] = player
+        await client.send_message(message.channel, embed=discord.Embed(description="재생한다!!!!"))
+        print(player.is_playing())
+        player.start()
+
+
+
+
+    if message.content.startswith("!일시정지"):
+        id = message.server.id
+        await client.send_message(message.channel, embed=discord.Embed(description="장비를 정비합니다"))
+        players[id].pause()
+
+    if message.content.startswith("!다시재생"):
+        id = message.server.id
+        await client.send_message(message.channel, embed=discord.Embed(description="다시재생한다!!!!"))
+        players[id].resume()
+
+    if message.content.startswith("!멈춰"):
+        id = message.server.id
+        await client.send_message(message.channel, embed=discord.Embed(description="세계의 시간은 멈춰있다..."))
+        players[id].stop()
+        print(players[id].is_playing())
+
+    if message.content.startswith('!예약'):
+        msg1 = message.content.split(" ")
+        url = msg1[1]
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+        print(player)
+
+        if server.id in queues:
+            queues[server.id].append(player)
+            print('if 1 '+str(queues[server.id])) #queues배열 확인
+        else:
+            queues[server.id] = [player] #딕셔너리 쌍 추가
+            print('else 1' + str(queues[server.id]))#queues배열 확인
+        await client.send_message(message.channel,'예약 완료\n')
+        musiclist.append(url) #대기목록 링크
+
+
+    if message.content.startswith('!대기목록'):
+
+        server = message.server
+        msg1 = message.content.split(" ")
+        mList = msg1[1]
+        num = 0
+        bSize = len(musiclist)
+
+        if mList =='보기':
+            embed = discord.Embed(
+                title='대기중인 곡 들',
+                description='대기중.....',
+                colour=discord.Colour.blue()
+            )
+            for i in musiclist:
+                print('예약리스트 : ' + i)
+                embed.add_field(name='대기중인 곡', value=i, inline=False)
+            await client.send_message(message.channel, embed=embed)
+
+        if mList =='취소':
+            while num<bSize:
+                del musiclist[0]
+                num = num+1
+
+            del queues[server.id]
+            await client.send_message(message.channel,'예약중인 음악 모두 취소 완료')
+
 access_token = os.environ["BOT_TOKEN"]        
 client.run(access_token)

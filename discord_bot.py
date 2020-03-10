52d37ce1db3a9805d6472658b6cf09580c888e02
rapsby <gsu5022@gmail.com>
2020-03-10 20:15:17 +0900

    update bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -12,26 +12,22 @@ import sys
 import json
 import time
 import datetime
+import re
 
-
-countG = 0
 client = discord.Client()
-players = {}
-queues= {}
-musiclist=[]
-mCount=1
-searchYoutube={}
-searchYoutubeHref={}
-server = ''
-voice_client = ''
+que = {}
+playerlist = {}
+playlist = list()
+
 if not discord.opus.is_loaded():
     discord.opus.load_opus('opus')
-def check_queue(id):
-    if queues[id]!=[]:
-        player = queues[id].pop(0)
-        players[id] = player
-        del musiclist[0]
-        player.start()
+
+def queue(id): #음악 재생용 큐
+	if que[id] != []:
+		player = que[id].pop(0)
+		playerlist[id] = player
+		del playlist[0]
+		player.start()
 
 @client.event
 async def on_ready():
@@ -43,6 +39,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author == client.user: #봇이 채팅을 쳤을 때 명령어로 인식되지 않음
+        return
+
+    if '호준' in message.content:
+        await client.send_message(message.channel, '호준이는 바보')
+        
     if message.content.startswith("안녕"):
         #await client.send_message(message.channel, "윤호준바아보")
         embed = discord.Embed(
@@ -58,107 +60,81 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
         print('안녕')
 
-    if message.content.startswith("!들어와"):
-        channel = message.author.voice.voice_channel
+    if message.content.startswith("!음악"): #음성채널에 봇을 추가 및 음악 재생
+        msg = message.content.split(" ")
+        try:
+            url = msg[1]
+            url1 = re.match('(https?://)?(www\.)?((youtube\.(com))/watch\?v=([-\w]+)|youtu\.be/([-\w]+))', url) #정규 표현식을 사용해 url 검사
+            if url1 == None:
+                await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: url을 제대로 입력해주세요.",colour = 0x2EFEF7))
+                return
+        except IndexError:
+            await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: url을 입력해주세요.",colour = 0x2EFEF7))
+            return
+
+        channel = message.author.voice.voice_channel 
         server = message.server
         voice_client = client.voice_client_in(server)
 
-        voice_client = server.voice_client
-
-        if voice_client == None:
-            await client.send_message(message.channel, '들어왔습니다')
-            await client.join_voice_channel(channel)
-            print(voice_client)
-        else:
-            await client.send_message(message.channel, '봇이 이미 들어와있습니다.') 
-
-    if '호준' in message.content:
-        await client.send_message(message.channel, '바보')
-
-    if message.content.startswith("!나가"):
-        server = message.server
-        voice_client = client.voice_client_in(server)
-        if voice_client:
-            await client.send_message(message.channel, '성공 : ㅃ2') # 나가드림
+        if client.is_voice_connected(server) and not playerlist[server.id].is_playing(): #봇이 음성채널에 접속해있으나 음악을 재생하지 않을 때
             await voice_client.disconnect()
-        else:
-            await client.send_message(message.channel,'실패 : 좆까') # 원래나가있었음 바보녀석 니녀석의 죄는 "어리석음" 이라는 .것.이.다.
-            pass
-            
-            
-    if message.content.startswith("!재생"):
-        server = message.server
-        voice_client = client.voice_client_in(server)
-        msg1 = message.content.split(" ")
-        url = msg1[1]
-        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
-        players[server.id] = player
-        #await client.send_message(message.channel, embed=discord.Embed(description="재생한다!!!!"))
+        elif client.is_voice_connected(server) and playerlist[server.id].is_playing(): #봇이 음성채널에 접속해있고 음악을 재생할 때
+            player = await voice_client.create_ytdl_player(url,after=lambda:queue(server.id),before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+            if server.id in que: #큐에 값이 들어있을 때
+                que[server.id].append(player)
+            else: #큐에 값이 없을 때
+                que[server.id] = [player]
+            await client.send_message(message.channel, embed=discord.Embed(title=":white_check_mark: 추가 완료!",colour = 0x2EFEF7))
+            playlist.append(player.title) #재생목록에 제목 추가
+            return
+
+        try:
+            voice_client = await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument: #유저가 음성채널에 접속해있지 않을 때
+            await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 음성채널에 접속하고 사용해주세요.",colour = 0x2EFEF7))
+            return
+
+        try:
+            player = await voice_client.create_ytdl_player(url,after=lambda:queue(server.id),before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+            playerlist[server.id] = player
+            playlist.append(player.title)
+        except youtube_dl.utils.DownloadError: #유저가 제대로 된 유튜브 경로를 입력하지 않았을 때
+            await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 존재하지 않는 경로입니다.",colour = 0x2EFEF7))
+            await voice_client.disconnect()
+            return
         player.start()
 
-
-
-
-    if message.content.startswith("!일시정지"):
-        id = message.server.id
-        await client.send_message(message.channel, embed=discord.Embed(description="장비를 정비합니다"))
-        players[id].pause()
-
-    if message.content.startswith("!다시재생"):
-        id = message.server.id
-        await client.send_message(message.channel, embed=discord.Embed(description="다시재생한다!!!!"))
-        players[id].resume()
-
-    if message.content.startswith("!멈춰"):
-        id = message.server.id
-        await client.send_message(message.channel, embed=discord.Embed(description="세계의 시간은 멈춰있다..."))
-        players[id].stop()
-        print(players[id].is_playing())
-
-    if message.content.startswith('!예약'):
-        msg1 = message.content.split(" ")
-        url = msg1[1]
+    if message.content == "!종료": #음성채널에서 봇을 나가게 하기
         server = message.server
         voice_client = client.voice_client_in(server)
-        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
-        print(player)
 
-        if server.id in queues:
-            queues[server.id].append(player)
-            print('if 1 '+str(queues[server.id])) #queues배열 확인
-        else:
-            queues[server.id] = [player] #딕셔너리 쌍 추가
-            print('else 1' + str(queues[server.id]))#queues배열 확인
-        await client.send_message(message.channel,'예약 완료\n')
-        musiclist.append(url) #대기목록 링크
+        if voice_client == None: #봇이 음성채널에 접속해있지 않았을 때
+            await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 봇이 음성채널에 없어요.",colour = 0x2EFEF7))
+            return
+        
+        await client.send_message(message.channel, embed=discord.Embed(title=":mute: 채널에서 나갑니다.",colour = 0x2EFEF7)) #봇이 음성채널에 접속해있을 때
+        await voice_client.disconnect()
+
+    if message.content == "!스킵":
+        id = message.server.id
+        if not playerlist[id].is_playing(): #재생 중인 음악이 없을 때
+            await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 스킵할 음악이 없어요.",colour = 0x2EFEF7))
+            return
+        await client.send_message(message.channel, embed=discord.Embed(title=":mute: 스킵했어요.",colour = 0x2EFEF7))
+        playerlist[id].stop()
+
+    if message.content == "!목록":
+
+        if playlist == []:
+            await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 재생목록이 없습니다.",colour = 0x2EFEF7))
+            return
+
+        playstr = "```css\n[재생목록]\n\n"
+        for i in range(0, len(playlist)):
+            playstr += str(i+1)+" : "+playlist[i]+"\n"
+        await client.send_message(message.channel, playstr+"```")
 
 
-    if message.content.startswith('!대기목록'):
-
-        server = message.server
-        msg1 = message.content.split(" ")
-        mList = msg1[1]
-        num = 0
-        bSize = len(musiclist)
-
-        if mList =='보기':
-            embed = discord.Embed(
-                title='대기중인 곡 들',
-                description='대기중.....',
-                colour=discord.Colour.blue()
-            )
-            for i in musiclist:
-                print('예약리스트 : ' + i)
-                embed.add_field(name='대기중인 곡', value=i, inline=False)
-            await client.send_message(message.channel, embed=embed)
-
-        if mList =='취소':
-            while num<bSize:
-                del musiclist[0]
-                num = num+1
-
-            del queues[server.id]
-            await client.send_message(message.channel,'예약중인 음악 모두 취소 완료')
 
 access_token = os.environ["BOT_TOKEN"]        
 client.run(access_token)
